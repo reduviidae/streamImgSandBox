d49b33c2391cdd45ec2f98f3aa2e3ee86f0b67fb
reduviidae <wiljago@gmail.com>
2019-05-24 12:08:35 -0400

    still working on streaming

--- sharpStream.js
+++ sharpStream.js
@@ -1,25 +1,24 @@
 const http = require("http");
 const https = require("https");
-const fs = require("fs");
 const sharp = require("sharp");
 
-const newFrame = sharp({
-  create: {
-    width: 4200,
-    height: 4800,
-    channels: 4,
-    background: { r: 255, g: 255, b: 255, alpha: 1 }
-  }
-})
-
-const createFrame = (createStream, width, height) => {
+const createFrame = (width, height) => {
+  const newFrame = sharp({
+    create: {
+      width: width,
+      height: height,
+      channels: 4,
+      background: { r: 255, g: 255, b: 255, alpha: 1 }
+    }
+  })
 
   return new Promise((resolve, reject) => {
     const outPath = `./artframe-${width}x${height}.png`;
     console.log('WRITING', outPath);
-    createStream.toFile(outPath);
-    createStream.on('end', () => resolve(outPath));
+    newFrame.toFile(outPath);
+    newFrame.on('end', () => resolve(outPath));
   })
 }
-
+console.time('createFrame')
 createFrame(newFrame, 4200, 4800)
+console.timeEnd('createFrame')
